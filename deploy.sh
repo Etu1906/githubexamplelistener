72e7eea09d65579df49075120f7cc679f43a6ec4
ETU1906 <yoanrazafinjaka@gmail.com>
2023-11-12 10:30:35 +0300

    deploy 2

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,3 @@ clear
 cp -r /home/ralph/ralph/s5/Mr_Naina/deploiement/github_listener/bin /home/ralph/ralph/s5/Mr_Naina/deploiement/github_listener/app/WEB-INF/classes
 cd /home/ralph/ralph/s5/Mr_Naina/deploiement/github_listener/app
 jar cvf ../cluster.war .
-cd ..
