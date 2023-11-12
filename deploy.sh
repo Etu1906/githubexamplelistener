a9c28e38f3c47f450939c721e631d2768feb93a3
ETU1906 <yoanrazafinjaka@gmail.com>
2023-11-12 10:19:39 +0300

    novaina ny build deployy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
-cp -r bin app/WEB-INF/classes
+cd 
+cp -r "/home/ralph/ralph/s5/Mr_Naina/deploiement/github_listener/bin" "/home/ralph/ralph/s5/Mr_Naina/deploiement/github_listener/app/WEB-INF/classes"
 cd app
 jar cvf ../cluster.war .
 cd ..
-
