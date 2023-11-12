b06c617872a1c468c30838759341fa663c00c391
ETU1906 <yoanrazafinjaka@gmail.com>
2023-11-12 10:29:04 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 clear
-cd 
-cp -r "/home/ralph/ralph/s5/Mr_Naina/deploiement/github_listener/bin" "/home/ralph/ralph/s5/Mr_Naina/deploiement/github_listener/app/WEB-INF/classes"
-cd app
+cp -r /home/ralph/ralph/s5/Mr_Naina/deploiement/github_listener/bin /home/ralph/ralph/s5/Mr_Naina/deploiement/github_listener/app/WEB-INF/classes
+cd /home/ralph/ralph/s5/Mr_Naina/deploiement/github_listener/app
 jar cvf ../cluster.war .
 cd ..
