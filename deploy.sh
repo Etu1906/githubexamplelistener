dbffdc8ae06813cae27a8c93569af7cc82b24dc1
ETU1906 <yoanrazafinjaka@gmail.com>
2023-11-12 10:36:48 +0300

    deploy manaraka

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-cd /home/ralph/ralph/s5/Mr_Naina/deploiement/github_listenerprod/githubexamplelistener
+cd /home/ralph/ralph/s5/Mr_Naina/deploiement/github_listener/prod/githubexamplelistener
 cp -r "bin" "app/WEB-INF/classes"
 cd app
 jar cvf ../cluster.war .
