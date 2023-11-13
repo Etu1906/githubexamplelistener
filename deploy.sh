7faa06eeba64059facbb90ade749efb423630bdb
ETU1906 <yoanrazafinjaka@gmail.com>
2023-11-13 09:33:00 +0300

    commit man

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 clear
-cd /home/ralph/ralph/s5/Mr_Naina/deploiement/github_listener/prod/githubexamplelistener
+path="/home/ralph/ralph/s5/Mr_Naina/deploiement/github_listener/prod/githubexamplelistener"
+cd $path
 cp -r "bin" "app/WEB-INF/classes"
 cd app
 jar cvf ../cluster.war .
-cd ..
+cd ..Exception
